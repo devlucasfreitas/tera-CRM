4299af929152c5cab331d1c287b4ba6afbf13289
unknown <lucasf.freitas@outlook.com>
2022-08-22 18:33:01 -0300

    login de usuarios

--- login/login.js
+++ login/login.js
@@ -13,46 +13,26 @@ btn.addEventListener("click", () => {
 //LOGICA PARA EFETUAR LOGIN
 function entrar() {
   let email = document.querySelector("#email");
-  let emailLabel = document.querySelector("#emailLabel");
 
   let senha = document.querySelector("#senha");
-  let senhaLabel = document.querySelector("#senhaLabel");
 
-  let msgError = document.querySelector("#msgError");
+  fetch(`./../usuarios.json`)
+    .then((res) => res.json())
+    .then((dados) => {
+      usuarios(dados);
+    });
+  function usuarios(dados) {
+    let entrou = false;
 
-  //LOGICA PARA CHAMAR A LISTA DE USER
-  let listaUser = [];
-
-  //LOGICA PARA VALIDAÇÃO DE USER
-  let userValid = {
-    nome: "",
-    email: "",
-    telefone: "",
-    senha: "",
-  };
-
-  listaUser = JSON.parse(localStorage.getItem("listaUser"));
-
-  listaUser.forEach((item) => {
-    if (email.value == item.emailCad && senha.value == item.senhaCad) {
-      userValid = {
-        nome: item.nomeCad,
-        email: item.emailCad,
-        telefone: item.telefoneCad,
-        senha: item.senhaCad,
-      };
-    }
-  });
-
-  if (email.value == userValid.email && senha.value == userValid.senha) {
-    window.location.href = "http://127.0.0.1:5500/dashboard-crm/home.html";
-  } else {
-    emailLabel.setAttribute("style", "color: red");
-    email.setAttribute("style", "border-color: red");
-    senhaLabel.setAttribute("style", "color: red");
-    senha.setAttribute("style", "border-color: red");
-    msgError.setAttribute("style", "display: block");
-    msgError.innerHTML = "Usuário ou senha incorretos";
-    email.focus(); //VOLTA PARA O CAMPO EMAIL CASO DE ERRO NA AUTENTICAÇÃO
+    dados.forEach((dados) => {
+      const emailUsuario = dados.email;
+      const senhaUsuario = dados.senha;
+      if (email.value === emailUsuario && senha.value === senhaUsuario) {
+        entrou = true;
+        window.location.href = "./../dashboard-crm/home.html";
+      } else if (entrou === false) {
+        window.alert("Email ou senha incorretos");
+      }
+    });
   }
 }
